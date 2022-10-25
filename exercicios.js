441836e2d05fe1bd0295bebfcd76822628579ce2
devannovazzi <d_annovazzi@hotmail.com>
2022-10-25 15:14:42 -0300

    aula 1 de java script

---
+++ exercicios.js
@@ -0,0 +1,10 @@
+const myName = "Carolina";
+const birthCity = "São José do Rio Preto";
+let birthYear = 1986;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+console.log(birthYear);
