197f95bb721428ec231e3c130dfb97791ccc3e5f
devannovazzi <d_annovazzi@hotmail.com>
2022-10-25 15:58:15 -0300

    Condições If e Else

---
+++ aula3.js
@@ -0,0 +1,10 @@
+const nota = 72;
+if (nota >= 80) {
+    console.log("Parabéns, você foi aprovado!");
+}
+else if (nota < 80 && nota >= 60){
+    console.log("Voce está na lista de espera");
+}
+else if (nota < 60) {
+    console.log("Você foi reprovado");
+}
